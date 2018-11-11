ab9753d47e203315556f8e79d29f3865c66df78b
RationalCoding <thomasmullendesign@gmail.com>
2018-11-10 21:20:48 -0500

    fix model url

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const labels = require('./../models/coco_model/labels.json')
 const ColorHash = require('color-hash')
 const colorHash = new ColorHash()
 
-const MODEL_URL = '/models/coco_model/web_model/tensorflowjs_model.pb'
+const MODEL_URL = 'models/coco_model/web_model/tensorflowjs_model.pb'
 const WEIGHTS_URL = 'models/coco_model/web_model/weights_manifest.json'
 
 const THRESHOLD = 0.3
